4ab730f803d74cd9d555f92de601e0ad08b178cd
noemipeteilh <169151883+noemipeteilh@users.noreply.github.com>
2024-05-27 21:10:50 +0200

    Entrega acordada

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,13 +1,32 @@
 function add(){
+    contador++;
+    let suma = document.querySelector ("#number")
+    suma.innerHTML = contador;
     //Escribir tu código que aumenta
 }
 
 function reset(){
+    contador = 0;
+    let reseteo = document.querySelector ("#number");
+    reseteo.innerHTML = contador;
     //Escribir tu código que hace un reset a 0
 }
 
 function decrease(){
-    //Escribir tu código que resta
+    if (contador > 0){
+        contador--;
+        let resta = document.querySelector ("#number");
+        resta.innerHTML = contador;
+        //Escribir tu código que resta
+    }
+
+if (contador === 0){
+    contador = 0;
+    let resta = document.querySelector("#number");
+    resta.innerHTML = contador;
+};
 };
 
+
+let contador = 0;
 export {add, reset, decrease}
--- src/js/events.js
+++ src/js/events.js
@@ -1,4 +1,15 @@
 import {add, reset, decrease} from './counter.js'
 
+// Función para sumar
+let  sumar = document.querySelector("#add");
+sumar.addEventListener("click", add);
+
+// Función para resetear
+let  resetear = document.querySelector("#reset");
+resetear.addEventListener("click", reset);
+
+// Función para restar
+let  restar = document.querySelector("#decrease");
+restar.addEventListener("click",decrease);
 
 //Aquí estarán los eventos que ejecutarán cada una de las funciones realizadas
